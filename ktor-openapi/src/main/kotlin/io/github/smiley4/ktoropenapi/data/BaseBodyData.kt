3c3891297fc5c355214b8b0b24d8bdc1b085ae80
Lukas Ruegner <ruegnerlukas@gmail.com>
2025-01-07 01:16:22 +0100

    code cleanup, mark classes internal

--- ktor-openapi/src/main/kotlin/io/github/smiley4/ktoropenapi/data/BaseBodyData.kt
+++ ktor-openapi/src/main/kotlin/io/github/smiley4/ktoropenapi/data/BaseBodyData.kt
@@ -5,7 +5,7 @@ import io.ktor.http.ContentType
 /**
  * The common information for request and response bodies.
  */
-sealed class OpenApiBaseBodyData(
+sealed class BaseBodyData(
     val description: String?,
     val required: Boolean,
     val mediaTypes: Set<ContentType>,
@@ -21,7 +21,7 @@ class OpenApiSimpleBodyData(
     mediaTypes: Set<ContentType>,
     val type: TypeDescriptor,
     val examples: List<ExampleDescriptor>
-) : OpenApiBaseBodyData(description, required, mediaTypes)
+) : BaseBodyData(description, required, mediaTypes)
 
 
 /**
@@ -32,4 +32,4 @@ class OpenApiMultipartBodyData(
     required: Boolean,
     mediaTypes: Set<ContentType>,
     val parts: List<OpenApiMultipartPartData>
-) : OpenApiBaseBodyData(description, required, mediaTypes)
+) : BaseBodyData(description, required, mediaTypes)
